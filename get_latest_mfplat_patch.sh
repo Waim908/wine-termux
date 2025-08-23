be0ee4d1776cae9b1e3d763f8c2b673c4e031656
Waim908 <160860390+Waim908@users.noreply.github.com>
2025-08-23 21:17:31 +0800

    update scripe get_latest_mfplat_patch.sh

--- get_latest_mfplat_patch.sh
+++ get_latest_mfplat_patch.sh
@@ -21,3 +21,22 @@ echo "HACK补丁可以与上面两个补丁一起应用，也可以选择不用"
 if ! curl --progress-bar -o "${PWD}/proton_mf_hacks.patch" $RAW_URL/wine-tkg-patches/proton/proton_mf_hacks/proton_mf_hacks.patch ; then
     echo "HACK 补丁下载失败"
 fi
+echo "缺失文件夹补齐（from Proton）"
+RAW_URL_PROTON="https://raw.githubusercontent.com/ValveSoftware/wine/refs/heads/proton_10.0/dlls/winegstreamer/media-converter"
+rm -rf "${PWD}"/media-converter
+mkdir media-converter
+Curl () {
+    if ! curl --progress-bar -o "${PWD}/media-converter/$1" $RAW_URL_PROTON/$1; then
+        echo "media-converter/$1 下载失败"
+    fi
+}
+Curl audioconv.c
+Curl audioconvbin.c
+Curl fossilize.c
+Curl lib.c
+Curl media-converter.h
+Curl murmur3.c
+Curl protondemuxer.c
+Curl videoconv.c
+echo "复制文件夹"
+echo "cp -r -p media-converter/ /path/to/your/src/dlls/winegstreamer/"
