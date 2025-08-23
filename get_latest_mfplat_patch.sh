4100d298b7f4f54b8a52028ccb146b2534300057
Waim908 <160860390+Waim908@users.noreply.github.com>
2025-08-23 22:15:45 +0800

    script update

--- get_latest_mfplat_patch.sh
+++ get_latest_mfplat_patch.sh
@@ -21,31 +21,34 @@ echo "HACK补丁可以与上面两个补丁一起应用，也可以选择不用"
 if ! curl --progress-bar -o "${PWD}/proton_mf_hacks.patch" $RAW_URL/wine-tkg-patches/proton/proton_mf_hacks/proton_mf_hacks.patch ; then
     echo "HACK 补丁下载失败"
 fi
-echo "缺失文件夹补齐（from Proton）"
-RAW_URL_PROTON="https://raw.githubusercontent.com/ValveSoftware/wine/refs/heads/proton_10.0/dlls/winegstreamer/media-converter"
-if [[ -d "${PWD}"/media-converter ]]; then
-    read -n 1 -p "${PWD}/media-converter文件夹已经存在，是否重新下载？" yesno_dl
-    case $yesno_dl in
-        Y|y) echo "下载" ;;
-        N|n) echo "取消" 
-        exit 0 ;;
-        *) echo "取消"
-        exit 0 ;;
-    esac
-fi
-mkdir media-converter
-Curl () {
-    if ! curl --progress-bar -o "${PWD}/media-converter/$1" $RAW_URL_PROTON/$1; then
-        echo "media-converter/$1 下载失败"
-    fi
-}
-Curl audioconv.c
-Curl audioconvbin.c
-Curl fossilize.c
-Curl lib.c
-Curl media-converter.h
-Curl murmur3.c
-Curl protondemuxer.c
-Curl videoconv.c
-echo "复制文件夹"
-echo "cp -r -p media-converter/ /path/to/your/src/dlls/winegstreamer/"
+echo "需要创建文件夹mkdir /path/to/your/src/dlls/winegstreamer/media-converter"
+echo "然后相关补丁会自动生成文件在此文件夹"
+# 补丁文件已经自带文件生成
+# echo "缺失文件夹补齐（from Proton）"
+# RAW_URL_PROTON="https://raw.githubusercontent.com/ValveSoftware/wine/refs/heads/proton_10.0/dlls/winegstreamer/media-converter"
+# if [[ -d "${PWD}"/media-converter ]]; then
+#     read -n 1 -p "${PWD}/media-converter文件夹已经存在，是否重新下载？" yesno_dl
+#     case $yesno_dl in
+#         Y|y) echo "下载" ;;
+#         N|n) echo "取消" 
+#         exit 0 ;;
+#         *) echo "取消"
+#         exit 0 ;;
+#     esac
+# fi
+# mkdir media-converter
+# Curl () {
+#     if ! curl --progress-bar -o "${PWD}/media-converter/$1" $RAW_URL_PROTON/$1; then
+#         echo "media-converter/$1 下载失败"
+#     fi
+# }
+# Curl audioconv.c
+# Curl audioconvbin.c
+# Curl fossilize.c
+# Curl lib.c
+# Curl media-converter.h
+# Curl murmur3.c
+# Curl protondemuxer.c
+# Curl videoconv.c
+# echo "复制文件夹"
+# echo "cp -r -p media-converter/ /path/to/your/src/dlls/winegstreamer/"
