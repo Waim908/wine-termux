35c8536387de7af359194b1986a50b84be5bf27f
Waim908 <waimxj449@foxmail.com>
2026-02-25 20:28:26 +0800

    patch.sh: fix

--- patch.sh
+++ patch.sh
@@ -1,8 +1,9 @@
 export script_dir="$(dirname $(readlink -f "$0"))"
 apply_patch() {
   if [[ -d $script_dir/$1/$2 ]]; then
-    . $script_dir/$1/$2/___patch___.conf
+    . $script_dir/$1/$2/___patch___.conf $2
     for i in "${patchFileArry[@]}"; do
+      echo "Apply $1/$2/$i"
       if ! patch -p1 < $script_dir/$1/$2/$i; then
         echo "Apply $i for $1/$2 failed"
         return 1
@@ -12,7 +13,4 @@ apply_patch() {
     echo "No Version Patch files=>$1/$2"
   fi
 }
-
-if [[ $1 == proton ]]; then
-  apply_patch proton $2 || exit 1
-fi
+apply_patch $1 $2 || exit 1
