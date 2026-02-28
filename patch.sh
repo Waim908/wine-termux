800b68b93601a2b1e4dad61015398b458659953a
Waim908 <waimxj449@foxmail.com>
2026-03-01 01:56:22 +0800

    action: 加入wine tkg工作流测试

--- patch.sh
+++ patch.sh
@@ -1,5 +1,5 @@
 export script_dir="$(dirname $(readlink -f "$0"))"
-apply_patch() {
+proton_apply_patch() {
   if [[ -d $script_dir/$1/$2 ]]; then
     . $script_dir/$1/$2/___patch___.conf $3
     echo "${patchFileArry[@]}"
@@ -13,7 +13,46 @@ apply_patch() {
       fi
     done
   else
-    echo "No Version Patch files=>$1/$2"
+    echo "Not Version Patch files=>$1/$2"
   fi
 }
-apply_patch $1 $2 $3 || exit 1
+
+wine_apply_patch() {
+  if [[ -d $script_dir/$1 ]]; then
+    . $script_dir/$1/___patch___.conf $2
+    echo "${patchFileArry[@]}"
+    [[ ! -z ${patchFileArry[@]} ]] || exit 1
+    for i in "${patchFileArry[@]}"; do
+      [[ -f $script_dir/$1/$i ]] || exit 1
+      echo "Apply $1/$i"
+      if ! patch -p1 < $script_dir/$1/$i; then
+        echo "Apply $i for $1 failed"
+        return 1
+      fi
+    done
+  else
+    echo "Not Patch files=>$1"
+  fi
+}
+
+copy_patches() {
+  if [[ -d $script_dir/$1 ]]; then
+    . $script_dir/$1/___patch___.conf $2
+    echo "${patchFileArry[@]}"
+    [[ ! -z ${patchFileArry[@]} ]] || exit 1
+    for i in "${patchFileArry[@]}"; do
+      [[ -f $script_dir/$1/$i ]] || exit 1
+      echo "Copy $1/$i to $3"
+      cp $script_dir/$1/$i $3/
+    done
+  else
+    echo "Not Patch files=>$1"
+  fi
+}
+
+case $1 in
+proton) proton_apply_patch proton $2 || exit 1 ;;
+wine-tkg) wine_apply_patch wine-tkg-git-staging-ge $2 || exit 1 ;;
+wine-tlg-auto) copy_patches wine-tkg-git-staging-ge $2 $3 || exit 1 ;;
+;;
+esac
