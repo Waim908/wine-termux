285aac7162d5d68710bf15295c6a6341efba67d0
Waim908 <waimxj449@foxmail.com>
2026-03-01 23:50:10 +0800

    action: update

--- patch.sh
+++ patch.sh
@@ -7,7 +7,7 @@ proton_apply_patch() {
     for i in "${patchFileArry[@]}"; do
       [[ -f $script_dir/$1/$2/$i ]] || exit 1
       echo "Apply $1/$2/$i"
-      if ! patch -p1 < $script_dir/$1/$2/$i; then
+      if ! patch -p1 <$script_dir/$1/$2/$i; then
         echo "Apply $i for $1/$2 failed"
         return 1
       fi
@@ -25,7 +25,7 @@ wine_apply_patch() {
     for i in "${patchFileArry[@]}"; do
       [[ -f $script_dir/$1/$i ]] || exit 1
       echo "Apply $1/$i"
-      if ! patch -p1 < $script_dir/$1/$i; then
+      if ! patch -p1 <$script_dir/$1/$i; then
         echo "Apply $i for $1 failed"
         return 1
       fi
@@ -50,9 +50,29 @@ copy_patches() {
   fi
 }
 
+add_tkg_mfdxgi() {
+  if [[ -z $ENABLE_PROTON_MF ]] || [[ $ENABLE_PROTON_MF == false ]] || [[ $ENABLE_PROTON_MF == 0 ]]; then
+
+    ver=$2 # 10.14
+    major_ver=$(echo "$ver" | cut -d. -f1)
+    minor_ver=$(echo "$ver" | cut -d. -f2)
+
+    echo "mfdxgi: Add env \'\$WINE_DO_NOT_CREATE_DXGI_DEVICE_MANAGER\' only (no full proton mf patch)"
+
+    if [ "$major_ver" -eq 9 ]; then
+      cp "$script_dir/wine9_do_not_create_dxgi_device_manager.patch" $3/wine9_do_not_create_dxgi_device_manager.mylatepatch || exit 1
+    elif [ "$major_ver" -ge 10 ] && [ "$minor_ver" -ge 4 ]; then
+      cp "$script_dir/wine_do_not_create_dxgi_device_manager2.patch" $3/wine_do_not_create_dxgi_device_manager2.mylatepatch || exit 1
+    else
+      echo "error"
+    fi
+  fi
+}
+
 case $1 in
 proton) proton_apply_patch proton $2 || exit 1 ;;
-wine-tkg) wine_apply_patch wine-tkg-git-staging-ge $2 || exit 1 ;;
-wine-tlg-auto) copy_patches wine-tkg-git-staging-ge $2 $3 || exit 1 ;;
-;;
+wine-tkg) wine_apply_patch wine-tkg-git-staging-ge $2 || exit 1
+  add_tkg_mfdxgi ;;
+wine-tlg-auto) copy_patches wine-tkg-git-staging-ge $2 $3 || exit 1
+  add_tkg_mfdxgi ;;
 esac
